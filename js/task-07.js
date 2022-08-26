814db2fe4800830aea154dde59e349815aa2f68e
OleksandrPushOk <koshup90@gmail.com>
2022-08-26 17:11:55 +0300

    Made changes to homework 6

--- js/task-07.js
+++ js/task-07.js
@@ -2,8 +2,12 @@
 const fondSizeControlEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
+textEl.style.fontSize = `${fondSizeControlEl.value}px`;
+
 fondSizeControlEl.addEventListener("input", (event) => {
     textEl.style.fontSize = event.currentTarget.value +'px';
   });
 
- 
+
+
+
